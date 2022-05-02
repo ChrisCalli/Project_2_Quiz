1fa01398f406779298429101c60905dc120b1fbf
ChrisCalli <104333372+ChrisCalli@users.noreply.github.com>
2022-05-03 11:37:25 +1200

    Update howTo.js
    Easy as.

--- scripts/howTo.js
+++ scripts/howTo.js
@@ -1,2 +1,15 @@
 
-	
+//---------------< DECLARATIONS >---------------------
+
+// JavaScript Document
+
+"use strict";
+
+
+//------------------< SCRIPT START >------------------
+
+	backButton.addEventListener("click", function(e) {
+		window.location = "index.html";
+	});
+
+//-------------------< SCRIPT END >-----------------
